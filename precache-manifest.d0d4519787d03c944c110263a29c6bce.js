fd6ccdcbc21f5e49a3f48c88bb7256b1ab05255c
ajitkumar <ajit9.dutta@gmail.com>
2019-01-24 20:10:31 +0530

    Updates

---
+++ precache-manifest.d0d4519787d03c944c110263a29c6bce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends-redux/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "9e0b58d846368f426a94",
+    "url": "/robofriends-redux/static/js/runtime~main.9e0b58d8.js"
+  },
+  {
+    "revision": "e627f944ad541ed172bb",
+    "url": "/robofriends-redux/static/js/main.e627f944.chunk.js"
+  },
+  {
+    "revision": "df7c11afc4d770c109ab",
+    "url": "/robofriends-redux/static/js/1.df7c11af.chunk.js"
+  },
+  {
+    "revision": "e627f944ad541ed172bb",
+    "url": "/robofriends-redux/static/css/main.5ab24703.chunk.css"
+  },
+  {
+    "revision": "df7c11afc4d770c109ab",
+    "url": "/robofriends-redux/static/css/1.c4bd7a6c.chunk.css"
+  },
+  {
+    "revision": "becf43e359c0c4471db03d9679ab05d8",
+    "url": "/robofriends-redux/index.html"
+  }
+];
